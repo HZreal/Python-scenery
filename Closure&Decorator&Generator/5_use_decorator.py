64945614a9df7220b580c7c02f10eff7baa7e91c
HZreal <huangzhen_happy@163.com>
2022-05-17 18:04:25 +0800

    snowflake & redis basic、cluster、sentinel

--- Closure&Decorator&Generator/5_use_decorator.py
+++ Closure&Decorator&Generator/5_use_decorator.py
@@ -1,15 +1,20 @@
+import functools
 import time
+
 
 # 定义装饰器，计算work函数的执行时间
 def decorator(func):
     print('start decorate ----')
+
     def inner():
         begin = time.time()
-        func()         # 原work函数在此执行
+        func()  # 原work函数在此执行
         end = time.time()
         exe_time = end - begin
-        print(exe_time)
+        print('exe_time ---->  ', exe_time)
+
     return inner
+
 
 # @decorator 表示去执行装饰器函数，做了两件事：
 # 1. func=work          将work作为函数decorator的参数传入，返回inner
@@ -21,16 +26,30 @@ def work():
     for i in range(10):
         print(i)
 
-# work()
 
-print(work.__dict__)
-
+work()
 
 
 
 
+# 通用装饰器，返回inner
+def decorator2(func):
+    def inner(*args, **kwargs):
+        return func(*args, **kwargs)
+    return inner
 
+# 扩展逻辑(此方式不算是标准的装饰器，语法上可行，用@操作单纯的给原函数包装一下，增添额外的print操作)
+def wrapper(func):
+    print('wrapper some extra operation ...')
+    return func
 
+@wrapper
+# @decorator2
+def hello(a, b):
+    return a + b
+
+print(hello(1, 2))
+print(hello.__name__)          # wrapper装饰时输出hello，decorator2装饰时输出inner
 
 
 
--- redis_learn/redis-cluster.py
+++ redis_learn/redis-cluster.py
@@ -1,23 +1,37 @@
+# redis集群
+# 一般至少6个节点，其中3个主节点，3个从节点
+# 集成了哨兵，主节点宕机时自动故障转移
 
 from rediscluster import RedisCluster
 
-if __name__ == '__main__':
-    # 组织集群的host和端口
-    nodes = [{'host': '192.168.94.131', 'port': '7000'},
-             {'host': '192.168.94.131', 'port': '7001'},
-             {'host': '192.168.94.131', 'port': '7002'}]
+
+# redis集群的某几个节点
+nodes = [
+    {'host': '192.168.94.131', 'port': '7000'},
+    {'host': '192.168.94.131', 'port': '7001'},
+    {'host': '192.168.94.131', 'port': '7002'}
+]
+
+
+def redis_cluster():
+
 
     try:
         # 构建RedisCluster对象
-        src = RedisCluster(startup_nodes=nodes, decode_responses=True)
-        name1 = src.get('name')
-        print(name1)
-        result = src.set('name', 'liu')
-        print(result)
+        rc = RedisCluster(startup_nodes=nodes, decode_responses=True)
     except Exception as e:
         print(e)
-    name2 = src.get('name')
-    print(name2)
+
+    # RedisCluster对象当做普通redis连接操作即可
+    result = rc.set('name', 'liu')
+    print(result)
+    name = rc.get('name')
+    print(name)
+
+
+
+if __name__ == '__main__':
+    redis_cluster()
 
 
 
