1bdc82476ca995c1dd74ef1a788d6022b1bd01e2
HZreal <huangzhen_happy@163.com>
2023-04-21 10:53:09 +0800

    'etcd basic'

--- micro_service/etcd/_etcd.py
+++ micro_service/etcd/_etcd.py
@@ -1,6 +1,8 @@
 import etcd3
+# etcd3==0.12.0
+# grpcio==1.44.0 , 版本1.45.0有点冲突
 
-client = etcd3.client(host="192.168.1.7", port=2379)
+client = etcd3.client(host="192.168.1.7", port=2379, user='huang', password='Root123456')
 
 # set
 client.put("/name", "huang")
