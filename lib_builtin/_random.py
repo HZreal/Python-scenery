1e555e44cca506144721494fcd4ad618547d4fc5
HZreal <huangzhen_happy@163.com>
2023-09-21 15:02:10 +0800

    update _random.py

--- lib_builtin/_random.py
+++ lib_builtin/_random.py
@@ -1,18 +1,20 @@
 import random
 
-#
+# 随机获取 [0, 1) 区间的一个浮点数
 r = random.random()
 print(r)
 
-#
+# 随机取样
 choice = random.choice([1, 2, 3, 4])
 print(choice)
 
-#
-sample = random.sample([1, 2 ,3, 4, 5, 6, 7], 2)
+# 随机取样（k个）
+sample = random.sample([1, 2, 3, 4, 5, 6, 7], 2)
 print(sample)
 
-
-
+# 对数组元素随机调整顺序
+arr = [1, 2, 3, 4, 5, 6, 7]
+random.shuffle(arr)
+print('arr  ---->  ', arr)
 
 
